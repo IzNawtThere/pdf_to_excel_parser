91b2e85a51a8d30921d7c2d356d7576a4cf3aff4
Abhiraj <166433150+IzNawtThere@users.noreply.github.com>
2025-06-17 16:00:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
 # config.py
 
-pdf_path = r"your/path/here.pdf" # path to your input PDF
-output_path = r"your/output/path.xlsx" # output Excel file
+# FILE PATHS
+pdf_path = r"your/path/to/input.pdf"
+output_path = r"your/path/to/output.xlsx"
+column_overlay_path = r"your/path/to/column_overlay.png"
 
-col_positions = [ 
-    # manually estimated column x-coordinates
-] 
+# COLUMN COORDINATES (TO BE FILLED BY USER)
+col_positions = [
+    # replace this after running auto_column_detect.py
+]
 
+# HEADERS (manually edit these to match your PDF structure)
 column_headers = [
-    # headers for Excel export
-] 
+    # Your Excel headers here
+]
+
+# GLOBAL PARAMS (used by all modules)
+DPI = 400
+CUT_TOP, CUT_LEFT, CUT_RIGHT = 100, 100, 80
+SAVGOL_WIN, SAVGOL_POLY = 51, 3
+PEAK_PROM, PEAK_DIST = 30000, 50
